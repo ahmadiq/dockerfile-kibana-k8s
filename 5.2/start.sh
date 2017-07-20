00ac534de11a558d4ea198bc8096edfa253fb697
Ahmad Iqbal <ahmad@aurorasolutions.io>
2017-07-20 18:35:39 +0500

    fix start script

--- 5.2/start.sh
+++ 5.2/start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-if [[ -n "${ELASTICSEARCH_URL}" ]]; then
-  sed -i 's|^\(# \+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"${ELASTICSEARCH_URL}\""'|' /opt/kibana/config/kibana.yml
-elif [[ -n "${ELASTICSEARCH_SERVICE_NAME}" ]]; then
+echo "ELASTICSEARCH_URL = $ELASTICSEARCH_URL"
+echo "ELASTICSEARCH_SERVICE_NAME = $ELASTICSEARCH_SERVICE_NAME"
+echo "ELASTICSEARCH_SERVICE_NAME = $ELASTICSEARCH_SERVICE_NAME"
+echo "SVC_HOST = $SVC_HOST"
+echo "SVC_PORT = $SVC_PORT"
+
+if [[ -n "$ELASTICSEARCH_URL" ]]; then
+  sed -i 's|^\(# \+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"$ELASTICSEARCH_URL\""'|' /opt/kibana/config/kibana.yml
+elif [[ -n "$ELASTICSEARCH_SERVICE_NAME" ]]; then
     SVC_HOST=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_HOST
     SVC_PORT=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_PORT
     sed -i 's|^\(# \+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"http://${!SVC_HOST}:${!SVC_PORT}\""'|' /opt/kibana/config/kibana.yml
